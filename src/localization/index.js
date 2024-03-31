ae6ee3a24ff0ee1f8413487718dc7ffcbe066f6e
Khaled Rashad <khaledrashadd@outlook.com>
2024-03-31 23:18:44 +0200

    .

---
+++ src/localization/index.js
@@ -0,0 +1,26 @@
+export const messages = {
+  en: {
+    message: {
+      enterName: 'Enter your name',
+      name: 'Name',
+      create: 'Create',
+      nameValidation: 'Please enter your name',
+      cardMessage: 'A greeting card with your name',
+      download: 'Download',
+      share: 'Share',
+      redo: 'Try again',
+    },
+  },
+  ar: {
+    message: {
+      enterName: 'ادخل اسمك',
+      name: 'الاسم',
+      create: 'انشاء',
+      nameValidation: 'الرجاء ادخال الاسم',
+      cardMessage: 'بطاقة تهنئة بإسمك',
+      download: 'تحميل',
+      share: 'مشاركة',
+      redo: 'اعادة مره اخرى',
+    },
+  },
+};
--- src/main.js
+++ src/main.js
@@ -2,33 +2,7 @@ import { createApp } from 'vue';
 import './style.css';
 import App from './App.vue';
 import { createI18n } from 'vue-i18n';
-
-const messages = {
-  en: {
-    message: {
-      enterName: 'Enter your name',
-      name: 'Name',
-      create: 'Create',
-      nameValidation: 'Please enter your name',
-      cardMessage: 'A greeting card with your name',
-      download: 'Download',
-      share: 'Share',
-      redo:"Try again",
-    },
-  },
-  ar: {
-    message: {
-      enterName: 'ادخل اسمك',
-      name: 'الاسم',
-      create: 'انشاء',
-      nameValidation: 'الرجاء ادخال الاسم',
-      cardMessage: 'بطاقة تهنئة بإسمك',
-      download: 'تحميل',
-      share: 'مشاركة',
-      redo: 'اعادة مره اخرى',
-    },
-  },
-};
+import { messages } from './localization';
 
 const i18n = createI18n({
   locale: 'ar',
