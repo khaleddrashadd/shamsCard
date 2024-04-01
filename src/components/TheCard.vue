c5e165f47529a0c8657d5e7e7407e8add108604d
Khaled Rashad <khaledrashadd@outlook.com>
2024-04-01 17:31:30 +0200

    .

--- src/components/TheCard.vue
+++ src/components/TheCard.vue
@@ -1,22 +1,38 @@
 <template>
   <div
     class="card-wrapper-left"
-    ref="cardRef">
-    <div class="receiver-wrapper">
-      <p class="receiver-name">{{ personName }}</p>
-    </div>
-    <div
+    >
+    <div ref="cardRef"
       class="hide-mob"
       @click="handleChangeSrc">
       <img
         :src="imgSrc"
         alt="Shams-Eids" />
+      <div
+        class="receiver-wrapper"
+        :style="{ bottom: imgTop }">
+        <p
+          class="receiver-name"
+          :class="{ 'light-text': isLight }">
+          {{ personName }}
+        </p>
+      </div>
     </div>
-    <div class="show-mob">
+    <!-- <div class="show-mob">
       <img
         :src="imgSrc"
         alt="Shams-Eids" />
+            <div
+      class="receiver-wrapper"
+      :style="{ bottom: imgTop }">
+      <p
+        class="receiver-name"
+        :class="{ 'light-text': isLight }">
+        {{ personName }}
+      </p>
     </div>
+
+    </div> -->
 
     <div class="gallery-wrapper">
       <div
@@ -51,6 +67,9 @@
   const emit = defineEmits(['langChange', 'getWrapperElement']);
 
   const cardRef = ref(null);
+  const isLight = ref(false);
+  const imgTop = ref(props.language === 'ar' ? '20.5%' : '19.5%');
+  const imgRight = ref('30%');
 
   onMounted(() => {
     emit('getWrapperElement', cardRef.value);
@@ -58,48 +77,102 @@
 
   const imgsMap = {
     0: {
-      ar: img_1,
-      en: img_en_1,
+      ar: {
+        src: img_1,
+        right: '30%',
+        top: '20.5%',
+      },
+      en: {
+        src: img_en_1,
+        right: '30%',
+        top: '19.5%',
+      },
+      dark: false,
     },
     1: {
-      ar: img_2,
-      en: img_en_2,
+      ar: {
+        src: img_2,
+        right: '30%',
+        top: '17%',
+      },
+      en: {
+        src: img_en_2,
+        right: '30%',
+        top: '16%',
+      },
+      dark: true,
     },
     2: {
-      ar: img_3,
-      en: img_en_3,
+      ar: {
+        src: img_3,
+        right: '30%',
+        top: '26%',
+      },
+      en: {
+        src: img_en_3,
+        right: '30%',
+        top: '24%',
+      },
+      dark: false,
     },
     3: {
-      ar: img_4,
-      en: img_en_4,
+      ar: {
+        src: img_4,
+        right: '30%',
+        top: '8.5%',
+      },
+      en: {
+        src: img_en_4,
+        right: '30%',
+        top: '9%',
+      },
+      dark: true,
     },
     4: {
-      ar: img_5,
-      en: img_en_5,
+      ar: {
+        src: img_5,
+        right: '30%',
+        top: '12.5%',
+      },
+      en: {
+        src: img_en_5,
+        right: '30%',
+        top: '9%',
+      },
+      dark: false,
     },
   };
 
   const gallery = ref(
-    Object.values(imgsMap).map((item) => item[props.language])
+    Object.values(imgsMap).map((item) => item[props.language].src)
   );
 
   watch(
     () => props.language,
     (newVal) => {
-      gallery.value = Object.values(imgsMap).map((item) => item[newVal]);
+      gallery.value = Object.values(imgsMap).map((item) => item[newVal].src);
+      imgTop.value = newVal === 'ar' ? '20.5%' : '19.5%';
+      isLight.value = false;
     }
   );
 
-  const imgSrc = ref(imgsMap[0].ar);
+  const imgSrc = ref(imgsMap[0].ar.src);
   const imgIndex = ref(0);
   const imgsArr = Object.values(imgsMap);
 
   const handleChangeSrc = (src) => {
     imgSrc.value = src;
+    const index = Object.keys(imgsMap).find(
+      (key) => imgsMap[key][props.language].src === src
+    );
+    isLight.value = imgsMap[index].dark;
+
+    imgTop.value = imgsMap[index][props.language].top;
+    imgRight.value = imgsMap[index][props.language].right;
   };
 
   watchEffect(() => {
-    imgSrc.value = imgsArr[imgIndex.value][props.language];
+    imgSrc.value = imgsArr[imgIndex.value][props.language].src;
   });
 </script>
 <style scoped>
@@ -114,27 +187,25 @@
     max-width: 100%;
     height: 100%;
     max-width: 500px;
-    width: 500px;
+    /* width: 500px; */
   }
 
   .receiver-wrapper {
     position: absolute;
-    bottom: 40%;
-    right: 26%;
-    min-width: 239px;
   }
 
   .receiver-name {
     font-weight: 500;
     font-size: 16px;
-    line-height: 32px;
+    line-height: 18px;
     color: #08266e;
     margin: 0;
+    width: 100%;
     text-align: center;
-    white-space: nowrap;
+    white-space: wrap;
     overflow: hidden;
-    text-overflow: ellipsis;
     font-weight: bold;
+    display: inline-block;
   }
   .gallery-wrapper {
     display: flex;
@@ -146,7 +217,7 @@
     flex-direction: column;
   }
   @media (max-width: 1157px) {
-    .gallery-wrapper{
+    .gallery-wrapper {
       flex-direction: row;
     }
     .card-wrapper-left {
@@ -166,5 +237,9 @@
     height: 100%;
     border-radius: 24px;
     object-fit: cover;
+  }
+
+  .light-text {
+    color: #fff;
   }
 </style>
---
+++ src/pages/CardPage.vue
@@ -0,0 +1,79 @@
+<template>
+  <section
+    class="full-bg"
+    :dir="dirEl"
+    :lang="langEl">
+    <div class="card-wrapper">
+      <TheLangSwitcher
+        v-model="langEl"
+        @lang-change="handleChangeLang" />
+      <TheForm
+        @nameEnter="getName"
+        :language="langEl"
+        :wrapperElement />
+      <TheCard
+        @getWrapperElement="getWrapperElement"
+        @lang-change="handleChangeLang"
+        :personName="personName"
+        :language="langEl" />
+    </div>
+  </section>
+</template>
+<script setup>
+  import { useI18n } from 'vue-i18n';
+  import { ref } from 'vue';
+  import TheForm from '../components/TheForm.vue';
+  import TheCard from '../components/TheCard.vue';
+  import TheLangSwitcher from '../components/TheLangSwitcher.vue';
+
+  const { locale } = useI18n();
+
+  const language =
+    localStorage.getItem('lang') ||
+    window?.navigator?.language.toLocaleLowerCase().slice(0, 2) ||
+    'ar';
+
+  const dirEl = ref(language === 'ar' ? 'rtl' : 'ltr');
+  const langEl = ref(language);
+
+  locale.value = language;
+
+  const personName = ref('');
+  const wrapperElement = ref(null);
+  const getName = (name) => {
+    personName.value = name;
+  };
+
+  const handleChangeLang = () => {
+    locale.value = localStorage.getItem('lang') || 'ar';
+
+    langEl.value = locale.value;
+    dirEl.value = locale.value === 'ar' ? 'rtl' : 'ltr';
+  };
+
+  const getWrapperElement = (element) => {
+    wrapperElement.value = element;
+  };
+</script>
+
+<style scoped>
+  .card-wrapper {
+    width: 80%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: auto;
+    min-height: 100vh;
+    height: 100%;
+    flex-wrap: wrap;
+  }
+
+  .full-bg {
+    background-image: url('./images/pattern.png');
+    background-repeat: no-repeat;
+    background-size: cover;
+    min-height: 100vh;
+    background-position: 100% 100%;
+    height: 100%;
+  }
+</style>
