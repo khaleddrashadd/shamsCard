584efa11b0f5b9c04df6afa976e279322fcb9d6d
Khaled Rashad <khaledrashadd@outlook.com>
2024-06-02 16:55:25 +0300

    modified lang functionality

--- src/pages/CardPage.vue
+++ src/pages/CardPage.vue
@@ -38,10 +38,11 @@
 
   const { locale } = useI18n();
 
-  const language =
-    localStorage.getItem('lang') ||
-    window?.navigator?.language.toLocaleLowerCase().slice(0, 2) ||
-    'ar';
+  // const language =
+  //   localStorage.getItem('lang') ||
+  //   window?.navigator?.language.toLocaleLowerCase().slice(0, 2) ||
+  //   'ar';
+  const language = 'ar';
 
   const dirEl = ref(language === 'ar' ? 'rtl' : 'ltr');
   const langEl = ref(language);
@@ -81,7 +82,7 @@
   const createQR = (contact) => {
     if (!contact) return (qrData.value = '');
 
-    const { name, phoneNumber, email,position } = contact;
+    const { name, phoneNumber, email, position } = contact;
     const vCardData = `BEGIN:VCARD
 VERSION:4.0
 N:${name};;;
